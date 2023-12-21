d30232875e8bad9d713dcf0d4a238a4a75b7f79b
MrCat1122 <148979020+MrCat1122@users.noreply.github.com>
2023-12-21 13:34:44 +0600

    models.py

--- post/models.py
+++ post/models.py
@@ -16,3 +16,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['created_at']
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
