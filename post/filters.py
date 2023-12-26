d952476b2b52c6498fadee646d49b87e29a8398d
AitiGiG <akanybekov678@gmail.com>
2023-12-26 16:49:45 +0600

    finish fillters.py

--- post/filters.py
+++ post/filters.py
@@ -1,7 +1,14 @@
 import django_filters
+from rest_framework import filters
 from .models import Post
 
 class PostModelFilter(django_filters.FilterSet):
     class Meta:
         model = Post
-        fields = ['title']
+        fields = ['title', 'category']
+
+class CustomSearchFilter(filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        if request.query_params.get('title_only'):
+            return ['title']
+        return super().get_search_fields(view, request)
